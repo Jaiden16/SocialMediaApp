f5b09ab9633e8d5aed32907b1c04987592c8766e
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-11-10 14:42:07 -0500

    added side bar

--- client/pages/javascript/feed.js
+++ client/pages/javascript/feed.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Dom loaded')
     loadPosts();
+    navSlide();
 
     const postForm = document.querySelector('#addPostForm');
 
@@ -18,4 +19,28 @@ async function loadPosts() {
         listItem.innerText = `Post: ${post.body} Likes: ${post.likes} Views: ${post.views} \n\n`;
         postsList.appendChild(listItem);
     });
+}
+
+const navSlide = () => {
+    const user_profile = document.querySelector('.user-profile');
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    user_profile.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle('nav-active');
+
+    //animate links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        }
+    });
+
+    //user profile animation
+    user_profile.classList.toggle('toggle');
+});
+
 }
