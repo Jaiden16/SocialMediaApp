08127aea43b593c617c5a56e62266cdfc416e654
tbriany <brianytaveras@pursuit.org>
2019-11-13 01:12:16 -0500

    updated css for landing page

--- client/app.js
+++ client/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Dom loaded')
+    hideForm()
+    addEventListener()
 })
+
+const hideForm = () => {
+    let formDiv = document.querySelector("#formDiv")
+    formDiv.style.display = 'none'
+}
+
+const addEventListener = () => {
+    let signUpBtn = document.querySelector("#signup_big")
+    signUpBtn.addEventListener('click', displayForm)
+}
+
+const displayForm = () => {
+    let formDiv = document.querySelector("#formDiv")
+    formDiv.style.display = 'block'
+}
+
